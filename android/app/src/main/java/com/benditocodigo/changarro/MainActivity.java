ba51ac72fd7c2ac341b111c16f38903ca1c0066f
Mauro Nava Luevanos <mauronavaluevanos63@gmail.com>
2026-06-28 09:09:09 -0600

    feat: add Android platform support via Capacitor configuration and build files

---
+++ android/app/src/main/java/com/benditocodigo/changarro/MainActivity.java
@@ -0,0 +1,5 @@
+package com.benditocodigo.changarro;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.benditocodigo.changarro',
+  appName: 'Changarro',
+  webDir: 'dist'
+};
+
+export default config;
